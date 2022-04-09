f5af126c12c9f5b7ee7944ea66605dddd8e2e7a7
Akbar <aumetov7@gmail.com>
2022-04-09 17:38:22 +0600

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Акбар Уметов on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
